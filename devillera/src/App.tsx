9a1fe7dbd9591b717e3dd515e6c1887f89eb5e20
DanHumphry <nokla4137@naver.com>
2021-04-01 20:27:48 +0900

    first setting

---
+++ devillera/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import Nav from '@/components/Nav';
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+};
+
+export default App;
---
+++ devillera/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import App from '@/App';
+import reportWebVitals from './reportWebVitals';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+reportWebVitals();
